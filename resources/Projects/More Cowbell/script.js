fc1fbd96aba93459e59a1527b6b0afc2b44d3ec2
Joe <fernettejs@gmail.com>
2020-09-26 15:56:34 -0400

    Change to Cowbell, add links to all pages

---
+++ resources/Projects/More Cowbell/script.js
@@ -0,0 +1,45 @@
+    function play() {
+      const audio = document.getElementById("audio");
+      audio.currentTime = 0;  //ADD
+      audio.play();
+  };
+
+    function newStyle() {
+        let newColor = '';
+        let newFont = ''; 
+        let x = Math.floor(Math.random()*7); 
+        switch (x){
+          case 0:
+            newColor = 'red';
+            newFont = 'Times New Roman'; 
+            break;
+          case 1: 
+            newColor = 'orange';
+            newFont = 'Florence, cursive'; 
+            break;
+          case 2:
+            newColor = 'yellow';
+            newFont = 'Verdana';
+            break; 
+          case 3:
+            newColor= 'purple';
+            newFont = 'Courier New';
+            break
+          case 4:
+            newColor = 'cyan';
+            newFont = 'Georgia'; 
+            break;
+          case 5:
+              newColor = 'maroon';
+              newFont = 'Palatino';
+              break;
+          case 6: 
+              newColor = 'lime';
+              newFont = 'Impact';
+              break;
+        }
+        var elem = document.querySelector('input');
+        elem.style.color = newColor;
+        elem.style.fontFamily = newFont; 
+      };
+
